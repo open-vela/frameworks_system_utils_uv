ed01da3b872a30dd2ccfd3f2922d07a84bc80b19
zhouliang3 <zhouliang3@xiaomi.com>
2021-06-04 14:51:18 +0800

    libuv/ext: add members of devinfo module
    N/A
    
    Signed-off-by: zhouliang3 <zhouliang3@xiaomi.com>

--- src/uv_devinfo.c
+++ src/uv_devinfo.c
@@ -23,6 +23,13 @@
 #include <nuttx/config.h>
 #include <nuttx/version.h>
 #include <uv_ext.h>
+#include <fcntl.h>
+
+#if CONFIG_ARCH_BOARD_SIM
+#include <nuttx/video/fb.h>
+#elif CONFIG_ARCH_BOARD_CUSTOM
+#include <nuttx/lcd/lcd_dev.h>
+#endif
 
 /****************************************************************************
  * Preprocessor Definitions
@@ -51,14 +58,19 @@
 #ifndef CONFIG_PRODUCT_NAME
 #define CONFIG_PRODUCT_NAME "Dev Product"
 #endif
-
+#define DEVINFO_BARND 1
 /****************************************************************************
  * Public Function
  ****************************************************************************/
 
 int uv_get_devinfo(uv_devinfo_t *devinfo)
 {
-  DEBUGASSERT(devinfo != NULL);
+  int fd, ret;
+  struct fb_videoinfo_s videinfo;
+
+  if (!devinfo) {
+    return UV_EINVAL;
+  }
 
   devinfo->brand = CONFIG_PRODUCT_BRAND;
   devinfo->device_type = CONFIG_PRODUCT_DEVICE_TYPE;
@@ -67,7 +79,40 @@ int uv_get_devinfo(uv_devinfo_t *devinfo)
   devinfo->product = CONFIG_PRODUCT_NAME;
   devinfo->os_type = "RTOS";
   devinfo->os_version_name = CONFIG_VERSION_STRING;
-  devinfo->platform_version_code = CONFIG_VERSION_BUILD;
+  devinfo->os_version_code = CONFIG_VERSION_BUILD;
+  devinfo->lanuage = "zh";
+  devinfo->region = "CN";
+
+#if CONFIG_ARCH_BOARD_SIM
+  fd = open("dev/fb0", O_RDWR);
+  if (fd < 0) {
+    return -errno;
+  }
+
+  ret = ioctl(fd, FBIOGET_VIDEOINFO, &videinfo);
+  if (ret != 0) {
+    return ret;
+  }
+
+  devinfo->screenwidth = videinfo.xres;
+  devinfo->screenheight = videinfo.yres;
+#elif CONFIG_ARCH_BOARD_CUSTOM
+  fd = open("dev/lcd", O_RDWR);
+  if (fd < 0) {
+    return -errno;
+  }
+
+  ret = ioctl(fd, LCDDEVIO_GETVIDEOINFO, &videinfo);
+  if (ret != 0) {
+    return ret;
+  }
+
+  devinfo->screenwidth = videinfo.xres;
+  devinfo->screenheight = videinfo.yres;
+#else
+  devinfo->screenwidth = 0;
+  devinfo->screenheight = 0;
+#endif
 
   return 0;
 }
